d7eb7d54773915fb9f0beadbe761126144a524ab
Mbanks1 <michael1banks@icloud.com>
2021-01-13 14:27:36 -0400

    my things

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,11 +6,29 @@ import Login from "../Login/Login";
 import authService from "../../services/authService";
 import Users from '../Users/Users'
 import "./App.css";
+import { DefaultDeserializer } from "v8";
+
 
 class App extends Component {
   state = {
     user: authService.getUser(),
+    mikesThings: [
+      {
+        name: 'bag of butterflies',
+        image: 'https://www.wired.com/images_blogs/wiredscience/2009/09/butterflies.jpg',
+        attributes: ['fluttering','grace','awesomeness','gentle']
+      },{
+        name: 'surf n turf',
+        image: 'https://media-cdn.tripadvisor.com/media/photo-s/0f/a6/41/e5/surf-n-turf.jpg',
+        attributes: ['all you need','if you get to surfin, best believe you gon need the turfin']
+      },{
+        name: 'bongos',
+        image: 'https://media.guitarcenter.com/is/image/MMGS7//CP221-Tunable-Bongos-with-Bag/440683000000000-00-1600x1600.jpg',
+        attributes: ['beats are life']
+      }
+    ]
   };
+
 
   handleLogout = () => {
     authService.logout();
