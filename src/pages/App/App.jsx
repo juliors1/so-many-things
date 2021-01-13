d7add954ef81b766d9b9006a098c646432581832
Gonzalez32 <3232rose@gmail.com>
2021-01-13 12:45:38 -0600

    juan-things

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -4,27 +4,62 @@ import NavBar from "../../components/NavBar/NavBar";
 import Signup from "../Signup/Signup";
 import Login from "../Login/Login";
 import authService from "../../services/authService";
-import Users from '../Users/Users'
+import Users from "../Users/Users";
 import "./App.css";
 
 class App extends Component {
   state = {
     user: authService.getUser(),
+    juanThings: [
+      {
+        name: "FIFA21",
+        image:
+          "https://atalayar.com/sites/default/files/styles/foto_/public/noticias/Atalayar_FIFA%2021.jpg?itok=V4t36zZI",
+        attributes: [
+          "Player-online",
+          "goal too many times",
+          "Xboxone to PS5 Soon!",
+          "Fut",
+        ],
+      },
+      {
+        name: "Art",
+        image:
+          "https://media.vanityfair.com/photos/5e8f9f875752fb00088317c4/16:9/w_1280,c_limit/The-Art-of-Making-Art-About-a-Plague.jpg",
+        attributes: [
+          "never get tired of it",
+          "blue, white, black, red",
+          "ZOOM",
+        ],
+      },
+      {
+        name: "Mac BookPro",
+        image:
+          "https://techcrunch.com/wp-content/uploads/2019/11/MacBook-Pro-16-IMG_2820-1.jpeg",
+        attributes: ["fast", "sexy", "bugs", "ummmmm", "$$$$$$$$$"],
+      },
+    ],
+
     tylersThings: [
       {
-        name: 'husky',
-        image: 'https://images.unsplash.com/photo-1560830672-575980201c33?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHw%3D&w=1000&q=80',
-        attributes: ['such fluff', 'white when clean','much shedding']
-      }, {
-        name: 'siamese',
-        image: 'https://images.unsplash.com/photo-1523863745117-a610a34eb231?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjF8fHNpYW1lc2UlMjBjYXR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-        attributes: ['such anger', 'loves water', 'vocal']
-      },{
-        name: 'fiancee',
-        image: 'https://images.unsplash.com/photo-1565206077212-4eb48d41f54b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cmluZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
-        attributes: ['small', 'spunky', 'caring']
-      }
-    ]
+        name: "husky",
+        image:
+          "https://images.unsplash.com/photo-1560830672-575980201c33?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHw%3D&w=1000&q=80",
+        attributes: ["such fluff", "white when clean", "much shedding"],
+      },
+      {
+        name: "siamese",
+        image:
+          "https://images.unsplash.com/photo-1523863745117-a610a34eb231?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjF8fHNpYW1lc2UlMjBjYXR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        attributes: ["such anger", "loves water", "vocal"],
+      },
+      {
+        name: "fiancee",
+        image:
+          "https://images.unsplash.com/photo-1565206077212-4eb48d41f54b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cmluZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        attributes: ["small", "spunky", "caring"],
+      },
+    ],
   };
 
   handleLogout = () => {
@@ -38,10 +73,10 @@ class App extends Component {
   };
 
   render() {
-    const { user } = this.state
+    const { user } = this.state;
     return (
       <>
-        <NavBar user={this.state.user} handleLogout={this.handleLogout}/>
+        <NavBar user={this.state.user} handleLogout={this.handleLogout} />
         <Route
           exact
           path="/"
@@ -74,9 +109,7 @@ class App extends Component {
         <Route
           exact
           path="/users"
-          render={() =>
-            user ? <Users /> : <Redirect to="/login" />
-          }
+          render={() => (user ? <Users /> : <Redirect to="/login" />)}
         />
       </>
     );
