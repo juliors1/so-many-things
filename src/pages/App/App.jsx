81f939e15a2a409d19f09ad7e3be7d65cb709aa2
tpreston96 <tpreston96@gmail.com>
2021-01-13 11:00:30 -0600

    tylers things

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -10,6 +10,21 @@ import "./App.css";
 class App extends Component {
   state = {
     user: authService.getUser(),
+    tylersThings: [
+      {
+        name: 'husky',
+        image: 'https://images.unsplash.com/photo-1560830672-575980201c33?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHw%3D&w=1000&q=80',
+        attributes: ['such fluff', 'white when clean','much shedding']
+      }, {
+        name: 'siamese',
+        image: 'https://images.unsplash.com/photo-1523863745117-a610a34eb231?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjF8fHNpYW1lc2UlMjBjYXR8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        attributes: ['such anger', 'loves water', 'vocal']
+      },{
+        name: 'fiancee',
+        image: 'https://images.unsplash.com/photo-1565206077212-4eb48d41f54b?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cmluZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        attributes: ['small', 'spunky', 'caring']
+      }
+    ]
   };
 
   handleLogout = () => {
